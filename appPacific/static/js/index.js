dad9de9d127a8703a0e603a6ce00decd12717775
franpvp <franpvp.98@gmail.com>
2024-03-28 15:57:17 -0300

    Se agregan dos campos en barra de búsqueda

--- appPacific/static/js/index.js
+++ appPacific/static/js/index.js
@@ -6,39 +6,94 @@ btn.addEventListener('click', function() {
     sidebar.classList.toggle('active');
 });
 // Buscador Selector Hab, Cant Adultos y Niños
-let btn_sec = document.querySelector('.btn-sec');
-let contenido = document.querySelector('.box-btn-sec');
+let btn_adultos = document.querySelector('.btn-adultos');
+let btn_ninos = document.querySelector('.btn-ninos');
+let box_adultos = document.querySelector('.box-btn-adultos');
+let box_ninos = document.querySelector('.box-btn-ninos');
 
-btn_sec.addEventListener('click', function() {
+
+btn_adultos.addEventListener('click', function() {
     event.stopPropagation();
-    contenido.classList.toggle('active');
+    box_adultos.classList.toggle('active');
 });
 
-let btn_minus = document.querySelector('#btn-minus');
-let btn_plus = document.querySelector('#btn-plus');
+btn_ninos.addEventListener('click', function() {
+    event.stopPropagation();
+    box_ninos.classList.toggle('active');
+});
+
+let btn_minus_adultos = document.querySelector('#btn-minus-adultos');
+let btn_plus_adultos = document.querySelector('#btn-plus-adultos');
+let btn_minus_ninos = document.querySelector('#btn-minus-ninos');
+let btn_plus_ninos = document.querySelector('#btn-plus-ninos');
+
+
 let contador_adultos = document.querySelector('.contador-adultos');
-let contador = 0;
+let contador_ninos = document.querySelector('.contador-ninos');
+let contador_1 = 0;
+let contador_2 = 0;
 
 // Detectar clics fuera del botón
 window.addEventListener('click', function(event) {
     // Verificar si el clic no se realizó dentro del contenedor del botón o su contenido
-    if (!btn_sec.contains(event.target)) {
+    if (!btn_adultos.contains(event.target)) {
         // Ocultar el contenido aquí
-        contenido.classList.remove('active');
+        box_adultos.classList.remove('active');
+    } else{
+        box_adultos.classList.add('active');
+    }
+
+    if(!btn_ninos.contains(event.target)){
+        box_ninos.classList.remove('active');
+    } else{
+        box_ninos.classList.add('active');
+    }
+});
+// Contador Adultos
+btn_minus_adultos.addEventListener('click', function() {
+    if(contador_1 > 0){
+        contador_1--;
+        contador_adultos.textContent = contador_1;
+        if(contador_1 != 1){
+            btn_adultos.textContent = "Adultos: " + contador_1;
+        } else{
+            btn_adultos.textContent = "Adulto: " + contador_1;
+        }
+        
     }
 });
 
-btn_minus.addEventListener('click', function() {
-    if(contador > 0){
-        contador--;
-        contador_adultos.textContent = contador;
-        btn_sec.textContent = contador;
-
+btn_plus_adultos.addEventListener('click', function() {
+    contador_1++;
+    contador_adultos.textContent = contador_1;
+    if(contador_1 != 1){
+        btn_adultos.textContent = "Adultos: " + contador_1;
+    } else{
+        btn_adultos.textContent = "Adulto: " + contador_1;
+    }
+    
+});
+// Contador Niños
+btn_minus_ninos.addEventListener('click', function() {
+    if(contador_2 > 0){
+        contador_2--;
+        contador_ninos.textContent = contador_2;
+        if(contador_2 != 1){
+            btn_ninos.textContent = "Niños: " + contador_2;
+        } else{
+            btn_ninos.textContent = "Niño: " + contador_2;
+        }
+        
     }
 });
 
-btn_plus.addEventListener('click', function() {
-    contador++;
-    contador_adultos.textContent = contador;
-    btn_sec.textContent = contador;
+btn_plus_ninos.addEventListener('click', function() {
+    contador_2++;
+    contador_ninos.textContent = contador_2;
+    if(contador_2 != 1){
+        btn_ninos.textContent = "Niños: " + contador_2;
+    } else{
+        btn_ninos.textContent = "Niño: " + contador_2;
+    }
+    
 });
