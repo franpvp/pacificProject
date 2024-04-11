4917848a8154370f693257b8c86c630e69849346
Marco <mar.bustosb@duocuc.cl>
2024-04-11 09:27:07 -0400

    se añaden estilos al restablecimiento

--- appPacific/urls.py
+++ appPacific/urls.py
@@ -34,8 +34,8 @@ urlpatterns = [
     path('gestion_usuarios/modificar_usuario/<int:id_usuario>/', modificar_usuario_admin, name='modificar_usuario_admin'),
     path('gestion_usuarios/eliminar_usuario/<int:id_usuario>/', eliminar_usuario_admin, name='eliminar_usuario_admin'),
     path('cerrarsesionadmin/', views.cerrarsesionadmin, name='cerrarsesionadmin'),
-    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="recuperacion/a-reset.html"), name="reset_password"),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="recuperacion/b-done.html"), name="password_reset_done"),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="recuperacion/c-confirm.html"), name="password_reset_confirm"),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="recuperacion/d-complete.html"), name="password_reset_complete"),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="recuperacion/reset_password.html"), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="recuperacion/reset_password_sent.html"), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="recuperacion/reset_password_form.html"), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="recuperacion/reset_password_complete.html"), name="password_reset_complete"),
 ]
