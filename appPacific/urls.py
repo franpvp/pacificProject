445bd1a3cfee35042244c84c1541c2f4f708abe8
franpvp <franpvp.98@gmail.com>
2024-04-29 14:24:12 -0400

    Se actualiza url.py

--- appPacific/urls.py
+++ appPacific/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
-from .views import crear_reserva_pacific_vendedor, eliminar_reserva_pacific_vendedor, gestion_reservas_vendedor, iniciosesion, cerrarsesion, misreservas, misdatos, administrador_home, crear_habitacion, crear_reserva_pacific, crear_usuario_admin, eliminar_habitacion, eliminar_reserva_pacific, eliminar_usuario_admin, gestion_habitaciones, gestion_reservas, gestion_usuarios, index, home, login, modificar_habitacion, modificar_reserva_pacific, modificar_reporte_reserva, modificar_reserva_pacific_vendedor, modificar_usuario_admin, registro, habitaciones, metodo_pago, transferencias, reserva_realizada, contacto, nosotros, tipo_usuario_admin, vendedor_home, ver_calendario_pacific, ver_calendario_pacific_vendedor, ver_habitacion, ver_reserva_pacific, ver_reporte_pacific, ver_reserva_pacific_vendedor, ver_usuarios_admin
+from .views import ReservaListCreateAPIView, crear_reserva_pacific_vendedor, eliminar_reserva_pacific_vendedor, gestion_reservas_vendedor, iniciosesion, cerrarsesion, misreservas, misdatos, administrador_home, crear_habitacion, crear_reserva_pacific, crear_usuario_admin, eliminar_habitacion, eliminar_reserva_pacific, eliminar_usuario_admin, gestion_habitaciones, gestion_reservas, gestion_usuarios, index, home, login, modificar_habitacion, modificar_reserva_pacific, modificar_reporte_reserva, modificar_reserva_pacific_vendedor, modificar_usuario_admin, modificar_usuario_admin, registro, habitaciones, metodo_pago, transferencias, reserva_realizada, contacto, nosotros, tipo_usuario_admin, vendedor_home, ver_calendario_pacific, ver_calendario_pacific_vendedor, ver_habitacion, ver_reserva_pacific, ver_reserva_pacific_vendedor, ver_usuarios_admin
+from .views import iniciosesion, cerrarsesion, misreservas, misdatos, administrador_home, crear_habitacion, crear_reserva_pacific, crear_usuario_admin, eliminar_habitacion, eliminar_reserva_pacific, eliminar_usuario_admin, gestion_habitaciones, gestion_reservas, gestion_usuarios, index, home, login, modificar_habitacion, modificar_reserva_pacific, modificar_reporte_reserva, modificar_usuario_admin, modificar_usuario_admin, registro, habitaciones, metodo_pago, transferencias, reserva_realizada, contacto, nosotros, tipo_usuario_admin, ver_calendario_pacific, ver_habitacion, ver_reserva_pacific, ver_usuarios_admin
 from django.contrib.auth import views as auth_views
 from django.contrib.auth import views as auth_views
-from .views import (MetodoPagoListCreate, 
-                    ReservaListCreate, ReporteReservaListCreate, TipoHabitacionListCreate, 
-                    HabitacionListCreate, DatosBancariosListCreate)
+from .views import (MetodoPagoListCreate, ReporteReservaListCreate, TipoHabitacionListCreate, 
+                    HabitacionListView, DatosBancariosListCreate)
 
 urlpatterns = [
     path('', index, name="index"),
@@ -30,12 +30,10 @@ urlpatterns = [
     path('gestion_reservas/', gestion_reservas, name="gestion_reservas"),
     path('crear_reserva_pacific/', crear_reserva_pacific, name="crear_reserva_pacific"),
     path('eliminar_reserva_pacific/', eliminar_reserva_pacific, name="eliminar_reserva_pacific"),
-    path('eliminar_reserva_pacific/<int:id_reserva>/', eliminar_reserva_pacific, name="eliminar_reserva_pacific"),
     path('modificar_reserva_pacific/', modificar_reserva_pacific, name="modificar_reserva_pacific"),
     path('modificar_reporte_reserva/', modificar_reporte_reserva, name="modificar_reporte_reserva"),
     path('ver_calendario_pacific/', ver_calendario_pacific, name="ver_calendario_pacific"),
     path('ver_reserva_pacific/', ver_reserva_pacific, name="ver_reserva_pacific"),
-    path('ver_reporte_pacific/', ver_reporte_pacific, name="ver_reporte_pacific"),
     path('gestion_usuarios/', gestion_usuarios, name="gestion_usuarios"),
     path('gestion_usuarios/crear_usuario/', crear_usuario_admin, name='crear_usuario_admin'),
     path('gestion_usuarios/ver_usuario/', ver_usuarios_admin, name='ver_usuarios_admin'),
@@ -58,9 +56,9 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="recuperacion/reset_password_form.html"), name="password_reset_confirm"),
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="recuperacion/reset_password_complete.html"), name="password_reset_complete"),
     path('metodo_pago/', MetodoPagoListCreate.as_view(), name='metodo-pago-list-create'),
-    path('reserva/', ReservaListCreate.as_view(), name='reserva-list-create'),
+    path('reserva/', ReservaListCreateAPIView.as_view(), name='reserva-list-create'),
     path('reporte_reserva/', ReporteReservaListCreate.as_view(), name='reporte-reserva-list-create'),
     path('tipo_habitacion/', TipoHabitacionListCreate.as_view(), name='tipo-habitacion-list-create'),
-    path('habitacion/', HabitacionListCreate.as_view(), name='habitacion-list-create'),
+    path('habitacion/', HabitacionListView.as_view(), name='habitacion-list-create'),
     path('datos_bancarios/', DatosBancariosListCreate.as_view(), name='datos-bancarios-list-create'),
 ]
